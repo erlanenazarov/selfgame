9089b2416b0edcb46ea1e18d42450b45c2c5d6ab
erlan <admin@abovethesky.ru>
2016-11-08 15:01:44 +0600

    + Done project

--- public/js/core.js
+++ public/js/core.js
@@ -1,11 +1,11 @@
 /**
  * Created by erlan on 11/7/16.
  */
-g
+
 var person_questions = {
     10: {
         'question': 'Кто основатель Microsoft?',
-        'answer': 'Бил Гейтс и Пол Аллен'
+        'answer': 'Билл Гейтс и Пол Аллен'
     },
     20: {
         'question': 'Стив Джобс был программистом?',
@@ -54,66 +54,66 @@ var phone_questions = {
         'answer': 'iPhone 3Gs'
     },
     20: {
-        'question': 'К какой компании пренадлежит Android Corporation',
-        'answer': 'Google'
+        'question': '<img src="public/images/questions/phone_nokia_310.jpg" style="width: 150px;" />',
+        'answer': 'Nokia 3250'
     },
     30: {
-        'question': 'С какого города автор логотипа Android? (Ирина Блок)',
-        'answer': 'Из Ленинграда'
+        'question': '<img src="public/images/questions/nokian95.jpg" style="width: 450px;" />',
+        'answer': 'Nokia N95'
     },
     40: {
-        'question': 'На продукты каких компаний, помимо Apple, устанавливается операционная система iOS?',
-        'answer': 'Не на каких продуктах'
+        'question': '<img src="public/images/questions/nokia3310.jpg" style="width: 450px;" />',
+        'answer': 'Nokia 3310'
     },
     50: {
-        'question': 'Эта ОС была создана финским программистом.',
-        'answer': 'OS Linux'
+        'question': '<img src="public/images/questions/samsungs3.jpg" style="width: 450px;" />',
+        'answer': 'Samsung Galaxy S3'
     }
 };
 
 var other_questions = {
     10: {
-        'question': 'Какая версия Windows вышла после 8.1',
-        'answer': '10. Видимо, у людей в Microsoft после идёт 10'
+        'question': 'Сколько видов одежды у Билла Гейтса?',
+        'answer': '1 вид одежды'
     },
     20: {
-        'question': 'К какой компании пренадлежит Android Corporation',
-        'answer': 'Google'
+        'question': 'Каким названием нельзя назвать папку в ОС Windows?',
+        'answer': '"Con". Билла Гейтса обзывали так в детстве'
     },
     30: {
-        'question': 'С какого города автор логотипа Android? (Ирина Блок)',
-        'answer': 'Из Ленинграда'
+        'question': 'Почему дизайн Facebook выполнен в синей гамме?',
+        'answer': 'Потому что, Марк страдает дальтонизмом, плохо различая оттенки красного и зеленого цветов'
     },
     40: {
-        'question': 'На продукты каких компаний, помимо Apple, устанавливается операционная система iOS?',
-        'answer': 'Не на каких продуктах'
+        'question': 'Сколько завпросов Google обрабатывает каждый день?',
+        'answer': 'Около миллиона'
     },
     50: {
-        'question': 'Эта ОС была создана финским программистом.',
-        'answer': 'OS Linux'
+        'question': 'Сколько лет знаку @',
+        'answer': '500 лет'
     }
 };
 
 var logical_questions = {
     10: {
-        'question': 'Какая версия Windows вышла после 8.1',
-        'answer': '10. Видимо, у людей в Microsoft после идёт 10'
+        'question': 'Придумайте алгоритм для того, чтобы пожарить яичницу',
+        'answer': '1 - Включить плиту;<br /> 2 - Поставить сковороду;<br /> 3 - Налить масло;<br /> 4 - Разбить яйца;<br /> 5 - Насыпать соль;<br /> 6 - Готово'
     },
     20: {
-        'question': 'К какой компании пренадлежит Android Corporation',
-        'answer': 'Google'
+        'question': 'Как запихать слона в  холодильник?',
+        'answer': '1 - Открыть холодильник;<br /> 2 - Засунуть в него слона;<br /> 3 - Закрыть холодильник;'
     },
     30: {
-        'question': 'С какого города автор логотипа Android? (Ирина Блок)',
-        'answer': 'Из Ленинграда'
+        'question': 'Найдите отличие:<br /> <img src="public/images/questions/difference.png" style="width: 450px;" />',
+        'answer': 'Слово "отличие" написано внизу'
     },
     40: {
-        'question': 'На продукты каких компаний, помимо Apple, устанавливается операционная система iOS?',
-        'answer': 'Не на каких продуктах'
+        'question': 'Муравей ползёт по столбу вверх, высота столба 20 метров. Днём он поднимается на 2 метра, а ночью спускается на 1 метр. За сколько дней он доберётся до вершины?',
+        'answer': 'За 19 дней'
     },
     50: {
-        'question': 'Эта ОС была создана финским программистом.',
-        'answer': 'OS Linux'
+        'question': 'Кто сказал выражение "Тут дело не в сервисе?"',
+        'answer': 'Я'
     }
 };
 
@@ -161,6 +161,14 @@ function showModal(data) {
             question_owner = phone_questions;
             question_owner_data = 'Мобильные телефоны';
             break;
+        case 'other_questions':
+            question_owner = other_questions;
+            question_owner_data = 'Разные факты';
+            break;
+        case 'logical_questions':
+            question_owner = logical_questions;
+            question_owner_data = 'Логика';
+            break;
     }
 
     var ticket = question_owner[parseInt($(sender).attr('data-point'))].question;
@@ -188,10 +196,6 @@ function hideModal() {
 }
 
 $(document).ready(function() {
-    $(team_panel_button).on('click', function() {
-        ShowTeamPanel();
-    });
-
     $('.panel-dismiss').on('click', function() {
         HidePanel();
     });
@@ -206,7 +210,6 @@ $(document).ready(function() {
     $('#close-modal').on('click', function(event) {
         event.preventDefault();
         hideModal();
-        ShowTeamPanel();
     });
 
     $('#show-answer').on('click', function(e) {
@@ -219,7 +222,6 @@ $(document).ready(function() {
     $('.accept-answer').on('click', function(e) {
         e.preventDefault();
         hideModal();
-        ShowTeamPanel();
         $(sender).addClass('done');
     });
 });
